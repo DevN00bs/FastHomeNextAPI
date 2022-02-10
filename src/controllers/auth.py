342f5c418967563422da97c022e15f1c8da9a5cf
Brandom Rodriguez <brandomrobor@tuta.io>
2022-02-10 16:59:11 -0700

    Multiple corrections to the registration controller
    Apparently typed schemas are not dictionaries

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -1,12 +1,17 @@
-from ..models.auth import RegistrationRequest, User
-from werkzeug.security import generate_password_hash
 from mongoengine.errors import OperationError
+from werkzeug.security import generate_password_hash
+
+from ..models.auth import User
 
 
-def register_user(data: RegistrationRequest) -> bool:
-    hashed_passwd = generate_password_hash(data.password)
+def register_user(data) -> bool:
+    hashed_passwd = generate_password_hash(data["password"])
     try:
-        User(**data.__dict__, passwdHash=hashed_passwd).save()
+        User(
+            username=data["username"],
+            passwdHash=hashed_passwd,
+            email=data["email"]
+        ).save()
         return True
     except OperationError:
         return False
