961c75c54d5a1af434dcca09ce943f45995a6c8d
Brandom Rodriguez <brandomrobor@tuta.io>
2022-02-10 19:15:21 -0700

    Created login controller with account checks Currently we don't return anything because an enum cannot return data, so no tokens yet

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -1,5 +1,5 @@
 from mongoengine.errors import OperationError, NotUniqueError
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from ..models.auth import User
 from ..utils.enums import ControllerStatus
@@ -18,3 +18,21 @@ def register_user(data) -> ControllerStatus:
         return ControllerStatus.ALREADY_EXISTS
     except OperationError:
         return ControllerStatus.ERROR
+
+
+def log_in(data):
+    try:
+        user_data = User.objects(username=data["username"]).first()
+    except OperationError:
+        return ControllerStatus.ERROR
+
+    if user_data is None:
+        return ControllerStatus.WRONG_CREDS
+
+    if not user_data["is_verified"]:
+        return ControllerStatus.NOT_VERIFIED
+
+    if not check_password_hash(user_data["passwd_hash"], data["password"]):
+        return ControllerStatus.WRONG_CREDS
+
+    return ControllerStatus.SUCCESS
