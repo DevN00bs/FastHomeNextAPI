0942ac0631f2c37ff64ff22ce46de611c9f98308
Brandom Rodriguez <brandomrobor@tuta.io>
2022-02-10 19:17:26 -0700

    Created login route Like mentioned in the previous commit, we currently cannot return a token

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,8 +1,8 @@
 from apiflask import APIBlueprint, input, output, abort, doc
 from apiflask.schemas import Schema
 
-from ..controllers.auth import register_user
-from ..models.auth import RegistrationRequest
+from ..controllers.auth import register_user, log_in
+from ..models.auth import RegistrationRequest, LoginRequest, LoginResponse
 from ..utils.enums import ControllerStatus
 
 router = APIBlueprint("auth", __name__, "Authentication", url_prefix="/api/auth")
@@ -21,3 +21,20 @@ def create_user(data):
         abort(500)
 
     return ""
+
+
+@router.post("/login")
+@input(LoginRequest)
+@output(LoginResponse)
+def log_in_user(data):
+    result = log_in(data)
+    if result == ControllerStatus.ERROR:
+        abort(500)
+
+    if result == ControllerStatus.WRONG_CREDS:
+        abort(401)
+
+    if result == ControllerStatus.NOT_VERIFIED:
+        abort(403)
+
+    return {"token": "temptoken"}
