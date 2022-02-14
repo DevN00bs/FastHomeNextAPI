dca6b37a4936a60cc31194240e8c2039dc0f29ed
Pwma77 <brayanpaulsalas.77@gmail.com>
2022-02-14 13:29:46 -0700

    CRUD props before check - runs

---
+++ src/controllers/properties.py
@@ -0,0 +1,47 @@
+from mongoengine.errors import OperationError
+
+from ..models.properties import PropertyData
+from ..utils.enums import ControllerStatus
+
+#create
+def register_prop(data) -> ControllerStatus:
+    try:
+        PropertyData(
+            **data
+        ).save()
+        return ControllerStatus.SUCCESS
+    except OperationError:
+        return ControllerStatus.ERROR
+
+#read
+def all_props(data) -> ControllerStatus:
+    try:
+        prop_data = PropertyData.objects(address=data["address"])#just saved cuz maybe used later
+        PropertyData(
+            **data
+        )._get_collection(meta={"collection":"properties"})
+        return ControllerStatus.SUCCESS
+    except OperationError:
+        return ControllerStatus.ERROR
+
+#TODO: field search
+
+#update
+def update_prop(data) -> ControllerStatus:
+    try:
+        PropertyData(
+            **data
+        ).update()
+        return ControllerStatus.SUCCESS
+    except OperationError:
+        return ControllerStatus.ERROR
+
+#delete
+def delete_prop(data) -> ControllerStatus:
+    try:
+        PropertyData(
+            **data
+        ).delete()
+        return ControllerStatus.SUCCESS
+    except OperationError:
+        return ControllerStatus.ERROR
