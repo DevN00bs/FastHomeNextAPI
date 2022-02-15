ee28fabc5441bdb30f052552dcad7bed4756fdf4
Brandom Rodriguez <brandomrobor@tuta.io>
2022-02-15 16:47:47 -0700

    Created registration tests

--- test/auth.py
+++ test/auth.py
@@ -5,7 +5,7 @@ from jwt import decode
 from mongoengine import connect, disconnect
 from werkzeug.security import generate_password_hash
 
-from src.controllers.auth import log_in
+from src.controllers.auth import log_in, register_user
 from src.models.auth import User
 from src.utils.enums import ControllerStatus
 
@@ -52,3 +52,30 @@ class AuthTests(TestCase):
         })
 
         assert tuple_result[0] == ControllerStatus.WRONG_CREDS
+
+    def test_successful_registration(self):
+        result = register_user({
+            "username": "newuser",
+            "email": "new@example.net",
+            "password": self._password
+        })
+
+        assert result == ControllerStatus.SUCCESS
+
+    def test_repeated_username(self):
+        result = register_user({
+            "username": self._username,
+            "email": "new@example.net",
+            "password": self._password
+        })
+
+        assert result == ControllerStatus.ALREADY_EXISTS
+
+    def test_repeated_email(self):
+        result = register_user({
+            "username": "newuser",
+            "email": self._email,
+            "password": self._password
+        })
+
+        assert result == ControllerStatus.ALREADY_EXISTS
