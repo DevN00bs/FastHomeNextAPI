853425fae5249c04e867d5f30c631ba164effb11
Brandom Rodriguez <brandomrobor@tuta.io>
2022-02-14 18:41:58 -0700

    Refactored login test to use class variables

--- test/auth.py
+++ test/auth.py
@@ -11,26 +11,28 @@ from src.utils.enums import ControllerStatus
 
 
 class LoginTests(TestCase):
+    _username = "testuser"
+    _password = "testpass"
+    _email = "test@example.net"
+
     @classmethod
     def setUpClass(cls) -> None:
         connect("fast-home-test", host="mongomock://localhost")
         User(
-            username="testuser",
-            email="test@example.net",
-            passwd_hash=generate_password_hash("testpass")
+            username=cls._username,
+            email=cls._email,
+            passwd_hash=generate_password_hash(cls._password)
         ).save()
 
     @classmethod
     def tearDownClass(cls) -> None:
         disconnect()
 
-    @staticmethod
-    def test_successful_login():
+    def test_successful_login(self):
         tuple_result = log_in({
-            "username": "testuser",
-            "password": "testpass"
+            "username": self._username,
+            "password": self._password
         })
 
-        assert tuple_result[0] == ControllerStatus.SUCCESS and str(User.objects(username="testuser").first().id) == \
-               decode(
-                   tuple_result[1], environ["JWT_SECRET"], ["HS256"], audience="login")["id"]
+        assert tuple_result[0] == ControllerStatus.SUCCESS and str(User.objects(username=self._username).first().id) == \
+               decode(tuple_result[1], environ["JWT_SECRET"], ["HS256"], audience="login")["id"]
