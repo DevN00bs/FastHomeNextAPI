d7d4cbc963a6c5d619e69b45f8fdfdf81e9ab117
Brandom Rodriguez <66086473+BrandomRobor@users.noreply.github.com>
2022-02-14 23:06:33 -0700

    Initial CI/CD support (#3)
    * Created test for a successful login
    
    * Refactored login test to use class variables
    
    * Modified workflow to use unittest and added environment variables
    
    * Changed workflow to use pipenv action
    
    * Changed workflow to install development dependencies
    
    * Removed weird Docker action thingy
    
    * Removed linter for now
    
    * Readded development dependencies installation
    Oops...
    
    * Added variable to install dependencies next to project root
    
    * Revert to using pip on workflow
    
    * Save freeze output to a file
    CI/CD is free... right?
    
    * Removed conditional
    
    * Use third-party pipenv action
    
    * Quotes are important
    
    * Added mongomock as dev dependency and get rid of lockfile
    
    * Returned to good ol' script
    
    * Added package cache
    
    * Moved lock command to avoid unnecessary package resolving
    
    * Readded linter
    
    * Exclude virtual environment from linter
    
    * Make exceptions list a comma-separated values string

---
+++ test/auth.py
@@ -0,0 +1,38 @@
+from os import environ
+from unittest import TestCase
+
+from jwt import decode
+from mongoengine import connect, disconnect
+from werkzeug.security import generate_password_hash
+
+from src.controllers.auth import log_in
+from src.models.auth import User
+from src.utils.enums import ControllerStatus
+
+
+class LoginTests(TestCase):
+    _username = "testuser"
+    _password = "testpass"
+    _email = "test@example.net"
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        connect("fast-home-test", host="mongomock://localhost")
+        User(
+            username=cls._username,
+            email=cls._email,
+            passwd_hash=generate_password_hash(cls._password)
+        ).save()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        disconnect()
+
+    def test_successful_login(self):
+        tuple_result = log_in({
+            "username": self._username,
+            "password": self._password
+        })
+
+        assert tuple_result[0] == ControllerStatus.SUCCESS and str(User.objects(username=self._username).first().id) == \
+               decode(tuple_result[1], environ["JWT_SECRET"], ["HS256"], audience="login")["id"]
