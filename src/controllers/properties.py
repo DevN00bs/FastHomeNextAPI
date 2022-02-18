e9137be780562c2ab208791ed47d7bab609c5de4
Brandom Rodriguez <brandomrobor@tuta.io>
2022-02-17 17:47:32 -0700

    Replaced star imports with alias imports

--- src/controllers/properties.py
+++ src/controllers/properties.py
@@ -1,12 +1,12 @@
 from mongoengine.errors import OperationError
 
-from ..models.properties import *
+import src.models.properties as m
 from ..utils.enums import ControllerStatus
 
 
 def register_prop(data) -> ControllerStatus:
     try:
-        PropertyDoc(
+        m.PropertyDoc(
             **data
         ).save()
         return ControllerStatus.SUCCESS
@@ -14,9 +14,9 @@ def register_prop(data) -> ControllerStatus:
         return ControllerStatus.ERROR
 
 
-def all_props() -> tuple[ControllerStatus, list[NewProperty]]:
+def all_props() -> tuple[ControllerStatus, list[m.NewProperty]]:
     try:
-        data = PropertyDoc.objects
+        data = m.PropertyDoc.objects
         # Objects means all data in the db + (filters)
         return ControllerStatus.SUCCESS, data
     except OperationError:
@@ -26,7 +26,7 @@ def all_props() -> tuple[ControllerStatus, list[NewProperty]]:
 
 def update_prop(data) -> ControllerStatus:
     try:
-        PropertyDoc.objects(id=data["id"]).first().update(**data)  # Schema
+        m.PropertyDoc.objects(id=data["id"]).first().update(**data)  # Schema
         return ControllerStatus.SUCCESS
     except OperationError:
         return ControllerStatus.ERROR
@@ -34,7 +34,7 @@ def update_prop(data) -> ControllerStatus:
 
 def delete_prop(data) -> ControllerStatus:
     try:
-        PropertyDoc.objects(id=data["id"]).first().delete()
+        m.PropertyDoc.objects(id=data["id"]).first().delete()
         # First to avoid making useless lists
         return ControllerStatus.SUCCESS
     except OperationError:
