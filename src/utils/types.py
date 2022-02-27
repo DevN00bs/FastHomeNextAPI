8c1bd02872b3018c1118c8350e838e910bcd331a
Brandom Rodriguez <brandomrobor@tuta.io>
2022-02-26 23:50:00 -0700

    Added ID validator method

--- src/utils/types.py
+++ src/utils/types.py
@@ -1,5 +1,8 @@
 from typing import Literal
 
+from bson.objectid import ObjectId
+from marshmallow import ValidationError
+
 token_audiences: type = Literal["verify", "forgot"]
 
 allowed_mimetypes = [
@@ -7,3 +10,8 @@ allowed_mimetypes = [
     "image/png",
     "image/webp"
 ]
+
+
+def is_valid_id(value):
+    if not ObjectId.is_valid(value):
+        raise ValidationError("Not a valid ID")
